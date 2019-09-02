c46ecba6ae7951c396ecda7060ef8c719f106c39
root <root@beaglebone.localdomain>
2019-09-02 16:09:53 +0000

    Updated webserver

--- HomeSurveillanceApp/webServer.py
+++ HomeSurveillanceApp/webServer.py
@@ -5,5 +5,5 @@ PORT = 5000
 Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 
 httpd = SocketServer.TCPServer(("", PORT), Handler)
-print('serving at port:', PORT)
+print 'serving at port:', PORT
 httpd.serve_forever()
